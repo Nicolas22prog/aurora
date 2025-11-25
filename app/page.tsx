98d4c208d17704c0a4b62bbcb92e98ec79adbc0c
Nicolas22prog <nicolasmonteiro8@gmail.com>
2025-11-25 19:32:36 -0300

    aaaaa

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,8 @@
 import GrandPrizeRaffle from "@/components/grand-prize-raffle"
 
+export const dynamic = 'force-dynamic'
+export const revalidate = 0
+
 export default function Home() {
   return <GrandPrizeRaffle />
 }
